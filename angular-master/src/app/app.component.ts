b8b351f29e1f9c502811ef7716804e895cab35d9
yasmine-hamzaoui <131954284+yasmine-hamzaoui@users.noreply.github.com>
2026-02-02 23:43:38 +0100

    workshop2
    workshop2

---
+++ angular-master/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ListSuggestionComponent } from "./core/list-suggestion/list-suggestion.component";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myModuleApp';
+}
